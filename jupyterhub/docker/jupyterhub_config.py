da46120bdabfb8f21c778a5e0718286e5a327f16
Chris Harris <chris.harris@kitware.com>
2017-09-18 14:12:08 -0400

    Add docker-compose spec for jupyterhub

---
+++ jupyterhub/docker/jupyterhub_config.py
@@ -0,0 +1,90 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+# Configuration file for JupyterHub
+import os
+
+c = get_config()
+
+# We rely on environment variables to configure JupyterHub so that we
+# avoid having to rebuild the JupyterHub container every time we change a
+# configuration parameter.
+
+# Spawn single-user servers as Docker containers
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+# Spawn containers from this image
+c.DockerSpawner.container_image = os.environ['DOCKER_NOTEBOOK_IMAGE']
+# JupyterHub requires a single-user instance of the Notebook server, so we
+# default to using the `start-singleuser.sh` script included in the
+# jupyter/docker-stacks *-notebook images as the Docker run command when
+# spawning containers.  Optionally, you can override the Docker run command
+# using the DOCKER_SPAWN_CMD environment variable.
+# Connect containers to this Docker network
+network_name = os.environ['DOCKER_NETWORK_NAME']
+c.DockerSpawner.use_internal_ip = True
+c.DockerSpawner.network_name = network_name
+c.DockerSpawner.extra_host_config = { 'network_mode': network_name }
+girder_api_url = os.environ.get('GIRDER_API_URL', 'http://localhost:8080/api/v1')
+
+spawn_cmd = os.environ.get('DOCKER_SPAWN_CMD', "start-singleuser.sh")
+spawn_cmd += " --SingleUserNotebookApp.default_url=/lab"
+spawn_cmd += " --NotebookApp.contents_manager_class='girder_jupyter.contents.girderfilemanager.GirderFileManager'"
+spawn_cmd += " --GirderFileManager.api_url=%s" % girder_api_url
+
+c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd })
+
+# Connect containers to this Docker network
+c.DockerSpawner.use_internal_ip = True
+# Pass the network name as argument to spawned containers
+# Explicitly set notebook directory because we'll be mounting a host volume to
+# it.  Most jupyter/docker-stacks *-notebook images run the Notebook server as
+# user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
+# We follow the same convention.
+# Mount the real user's Docker volume on the host to the notebook user's
+# notebook directory in the container
+# Remove containers once they are stopped
+c.DockerSpawner.remove_containers = True
+# For debugging arguments passed to spawned containers
+c.DockerSpawner.debug = True
+
+# User containers will access hub by container name on the Docker network
+c.JupyterHub.hub_ip = 'jupyterhub'
+c.JupyterHub.hub_port = 8080
+
+# TLS config
+#c.JupyterHub.port = 443
+#c.JupyterHub.ssl_key = os.environ['SSL_KEY']
+#c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
+
+# Authenticate users with Girder
+c.JupyterHub.authenticator_class = 'girder_jupyterhub.auth.GirderAuthenticator'
+
+import sys
+c.GirderAuthenticator.api_url = girder_api_url
+
+# Persist hub data on volume mounted inside container
+data_dir = os.environ.get('DATA_VOLUME_CONTAINER', '/data')
+
+c.JupyterHub.cookie_secret_file = os.path.join(data_dir,
+    'jupyterhub_cookie_secret')
+
+c.JupyterHub.db_url = 'postgresql://postgres:{password}@{host}/{db}'.format(
+    host=os.environ['POSTGRES_HOST'],
+    password=os.environ['POSTGRES_PASSWORD'],
+    db=os.environ['POSTGRES_DB'],
+)
+
+# Whitlelist users and admins
+#c.Authenticator.whitelist = whitelist = set()
+#c.Authenticator.admin_users = admin = set()
+#c.JupyterHub.admin_access = True
+#pwd = os.path.dirname(__file__)
+#with open(os.path.join(pwd, 'userlist')) as f:
+#    for line in f:
+#        if not line:
+#            continue
+#        parts = line.split()
+#        name = parts[0]
+#        whitelist.add(name)
+#        if len(parts) > 1 and parts[1] == 'admin':
+#            admin.add(name)
