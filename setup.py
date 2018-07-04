090835ed7e75a253eff7904b40ec433a2e506377
Chris Harris <chris.harris@kitware.com>
2018-07-04 09:37:09 -0400

    Add command line tool

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ocdeploy',
+    version='0.0.1',
+    description='',
+    author='Kitware, Inc.',
+    author_email='kitware@kitware.com',
+    url='https://www.openchemistry.org/',
+    license='BSD 3-Clause',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD 3-Clause',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
+    ],
+    packages=find_packages(),
+    install_requires=['docker-compose', 'click'],
+    entry_points={
+        'console_scripts': [
+            'ocdeploy = cli:main'
+        ]
+    },
+
+)
