887a05d436161f33ff5a0ca04322ca6bea008b4d
Alessandro Genova <alessandro.genova@kitware.com>
2018-11-30 10:49:57 -0500

    Perform cjson to smiles conversion inside chemml container

--- docker/chemml/oc-chemml.py
+++ docker/chemml/oc-chemml.py
@@ -2,19 +2,46 @@ import sys
 import json
 from chemml.models.keras.trained import OrganicLorentzLorenz
 
+from openbabel import OBMol, OBConversion
+
+def ob_convert_str(str_data, in_format, out_format):
+    mol = OBMol()
+    conv = OBConversion()
+    conv.SetInFormat(in_format)
+    conv.SetOutFormat(out_format)
+    conv.ReadString(mol, str_data)
+
+    return (conv.WriteString(mol), conv.GetOutFormat().GetMIMEType())
+
+from avogadro.core import Molecule
+from avogadro.io import FileFormatManager
+
+def avo_convert_str(str_data, in_format, out_format):
+    mol = Molecule()
+    conv = FileFormatManager()
+    conv.readString(mol, str_data, in_format)
+    return conv.writeString(mol, out_format)
+
+def cjson_to_smiles(cjson):
+    cml = avo_convert_str(json.dumps(cjson), 'cjson', 'cml')
+    smiles, mime = ob_convert_str(cml, 'cml', 'smiles')
+    return smiles
+
 if len(sys.argv) == 1:
     raise Exception("Input File not provided")
 
-input_file = '/data/' + sys.argv[1]
+input_file = sys.argv[1]
 
 with open(input_file) as f:
-    smiles = f.read()
+    cjson = json.load(f)
+
+smiles = cjson_to_smiles(cjson)
 
 cls = OrganicLorentzLorenz()
 cls.load(summary=True)
 pred = cls.predict(smiles)
 
-if input_file.endswith('.in'):
+if input_file.lower().endswith('.in'):
     output_file = input_file[:-3] + '.out'
 else:
     output_file = input_file + '.out'
