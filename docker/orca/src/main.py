f469b441986bd1bbf35031893f5b7cb739a78e01
Muammar El Khatib <muammarelkhatib@gmail.com>
2019-05-31 09:26:50 -0700

    orca docker image is operational with openchemistry.io.orca.OrcaReader
    - The docker image is operational and compatible with
      oc.io.orca.OrcaReader.
    - Beautification of orca/src/*.py with black.

--- docker/orca/src/main.py
+++ docker/orca/src/main.py
@@ -5,6 +5,7 @@ import json
 import describe
 import run
 
+
 def describe_callback(ctx, param, value):
     if not value:
         return
@@ -13,31 +14,54 @@ def describe_callback(ctx, param, value):
     click.echo(description, file=sys.stdout)
     ctx.exit()
 
+
 @click.command()
-@click.option('--describe', '-d',
-              help='Return an object to std output that describes the expected input and output parameters of this container.',
-              is_flag=True, is_eager=True, expose_value=False, callback=describe_callback)
-@click.option('--geometry', '-g', 'geometry_file',
-              type=click.Path(exists=True, dir_okay=False, resolve_path=True),
-              required=True,
-              help='The path to the file containing the input geometry')
-@click.option('--parameters', '-p', 'parameters_file',
-              type=click.Path(exists=True, dir_okay=False, resolve_path=True),
-              required=True,
-              help='The path to the JSON file containing the input input parameters.')
-@click.option('--output', '-o', 'output_file',
-              type=click.Path(exists=False, dir_okay=False, resolve_path=True),
-              required=True,
-              help='The path to the file that will contain the converted calculation output.')
-@click.option('--scratch', '-s', 'scratch_dir',
-              type=click.Path(exists=False, dir_okay=True, file_okay=False, resolve_path=True),
-              required=True,
-              help='The path to the directory that will be used as scratch space while running the calculation.')
+@click.option(
+    "--describe",
+    "-d",
+    help="Return an object to std output that describes the expected input and output parameters of this container.",
+    is_flag=True,
+    is_eager=True,
+    expose_value=False,
+    callback=describe_callback,
+)
+@click.option(
+    "--geometry",
+    "-g",
+    "geometry_file",
+    type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+    required=True,
+    help="The path to the file containing the input geometry",
+)
+@click.option(
+    "--parameters",
+    "-p",
+    "parameters_file",
+    type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+    required=True,
+    help="The path to the JSON file containing the input input parameters.",
+)
+@click.option(
+    "--output",
+    "-o",
+    "output_file",
+    type=click.Path(exists=False, dir_okay=False, resolve_path=True),
+    required=True,
+    help="The path to the file that will contain the converted calculation output.",
+)
+@click.option(
+    "--scratch",
+    "-s",
+    "scratch_dir",
+    type=click.Path(exists=False, dir_okay=True, file_okay=False, resolve_path=True),
+    required=True,
+    help="The path to the directory that will be used as scratch space while running the calculation.",
+)
 def main(geometry_file, parameters_file, output_file, scratch_dir):
     with open(parameters_file) as f:
         params = json.load(f)
     run.run_calculation(geometry_file, output_file, params, scratch_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
