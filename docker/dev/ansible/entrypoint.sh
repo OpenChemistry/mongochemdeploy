85fe884bb8f6f6a8f91db7b6517c4e5b6ab7b7dd
Chris Harris <chris.harris@kitware.com>
2019-11-14 12:21:11 -0500

    Pass module path on command line
    With the latest version of ansible the girder module is not found
    so a directory to module path.

--- docker/dev/ansible/entrypoint.sh
+++ docker/dev/ansible/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ansible-galaxy install -r requirements.yml
-ansible-playbook -i inventory/localhost dev.yml
+ansible-playbook -M /root/.ansible/roles/girder.girder/library -i inventory/localhost dev.yml
